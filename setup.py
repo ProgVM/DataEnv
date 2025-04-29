dbfdbae1197b5cf743ba4b35a14d7702bba7a174
ProgVM <progvminc@gmail.com>
2025-04-29 20:41:29 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dataenv',
+    version='0.1',
+    packages=find_packages(),
+    description='The package for doing any operations with data.',
+    author='ProgVM',
+    author_email='progvm_inc@example.com',
+    url='https://github.com/progvm/dataenv',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
